56b18081f8c0351871fee800f3669bf62f7635ce
nadiaell <nadiaell@hotmail.com>
2025-02-09 21:49:49 -0300

    agregue botones enn javascript

---
+++ script.js
@@ -0,0 +1,54 @@
+function $(selector) {
+      return document.querySelector(selector);
+    }
+    
+    function $$(selector) {
+      return document.querySelectorAll(selector);
+    }
+    
+
+
+
+
+
+const $Balance = $("#Balance");
+const $Categoria = $("#Categoria");
+const $Reporte= $("#Reporte");
+const $seccionBalance =$("#seccionBalance")
+
+const $seccionFiltros=$("#seccionFiltros")
+const $seccionOperaciones=$("#seccionOperaciones")
+const $seccionCategoria =$("#seccionCategoria")
+const $seccionReporte =$("#seccionReporte")
+const $seccionNuevaOperacion= $("#seccionNuevaOperacion")
+
+
+
+
+$Balance.addEventListener("click", () => {
+      $seccionBalance.style.display = "block";
+      $seccionFiltros.style.display = "block";
+      $seccionNuevaOperacion.style.display = "block";
+      $seccionOperaciones.style.display = "block";
+      $seccionCategoria.style.display = "none";
+      $seccionReporte.style.display = "none";
+})
+$Categoria.addEventListener("click", () => {
+      $seccionCategoria.style.display = "block";
+      $seccionBalance.style.display = "none";
+      $seccionFiltros.style.display = "none";
+      $seccionNuevaOperacion.style.display = "none";
+      $seccionOperaciones.style.display = "none";
+      
+      $seccionReporte.style.display = "none";
+     
+})
+$Reporte.addEventListener("click", () => {
+      $seccionReporte.style.display = "block";
+      $seccionCategoria.style.display = "none";
+      $seccionBalance.style.display = "none";
+      $seccionFiltros.style.display = "none";
+      $seccionNuevaOperacion.style.display = "none";
+      $seccionOperaciones.style.display = "none";
+     
+})
