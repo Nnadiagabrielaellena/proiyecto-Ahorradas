2f489e409535606f6690fc03d1951f6fb4428a13
nadiaell <nadiaell@hotmail.com>
2025-02-10 20:51:33 -0300

    saque funciones

--- datos.js
+++ datos.js
@@ -1,7 +1,6 @@
 const datos = [
       {
-        id: "9e3f7ce4-b9a7-4244-b709-dae5c1f1d4a8",
-        name: "Harry Potter",
+        
        
         Descripcion:"",
         Categoria :"",
--- script.js
+++ script.js
@@ -66,31 +66,5 @@ $nuevaOperacion.addEventListener("click", () => {
       const $contenedorOperaciones = $("#contenedorOperaciones")
      
 })
-function pintarDatos(arrayPersonajes) {
-      $sectionContainCards.innerHTML = "";
-      for (const { image, name, species, house, gender, alive, dateOfBirth } of arrayPersonajes) {
-        $sectionContainCards.innerHTML += `<div class="flex flex-col justify-center items-center border border-2 border-black rounded-xl w-5/6 md:w-5/12 lg:w-3/12 mx-8 my-2 h-72">
-          <img src="${image}" class="h-24 w-24">
-          <p>Nombre: ${capitalize(name)}</p>
-          <div class="flex flex-row justify-around w-full flex-wrap">
-            <p class="w-2/5">Especie: ${capitalize(species)}</p>
-            <p class="w-2/5">Casa: ${capitalize(house)}</p>
-            <p class="w-2/5">Genero: ${capitalize(gender)}</p>
-            <p class="w-2/5">Nacimiento: ${dateOfBirth ? dateOfBirth : "Desconocido"}</p>
-            <p class="w-2/5">Vivo: ${alive ? "Si, esta vivito" : "No, se despidio"}</p>
-          </div>
-        </div>`;
-      }
-    }`            `
-    function pintarDatos(arrayPersonajes) {
-      $contenedorOperaciones.innerHTML = "";
-      for (const { Descripcion, Categoria, Fecha, Monto,} of arrayPersonajes) {
-        $contenedorOperaciones.innerHTML += `<div id ="contenedorOperaciones"class=" flex justify-between  imagenNuevaOperacion bg-[./Style/img/imagenNuevoGasto] bg-gray-500 h-[400px] w-11/12 text-lg p-8">
-        <p>Descripción</p>
-        <p>Categoría</p>
-        <p>Fecha</p>
-        <p>Monto</p>
-        <p>Acciones</p>
-  </div>`
-      }
-    }
+
+    
