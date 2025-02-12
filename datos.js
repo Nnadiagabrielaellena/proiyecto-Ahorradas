5f297e1dca2a8df1634a234c925d0e3fd219f267
nadiaell <nadiaell@hotmail.com>
2025-02-12 09:31:16 -0300

    modifique seccion de operaciones para responsive

--- datos.js
+++ datos.js
@@ -1,15 +1,3 @@
-const datos = [
-      {
-        
-       
-        Descripcion:"",
-        Categoria :"",
-        Fecha: "",
-        Monto :0,
-        Acciones :["Editar","Eliminar"],
 
-        
-        },
-      ];
 
       export default datos;
--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-let datos2 = [];
+const datos = [
+      {
+        
+       
+        Descripcion:"",
+        Categoria :"",
+        Fecha: "",
+        Monto :0,
+        Acciones :["Editar","Eliminar"],
+
+        
+        },
+        
+      ];
 
 function $(selector) {
       return document.querySelector(selector);
@@ -13,9 +26,10 @@ function $(selector) {
 
 
 
-const $Balance = $("#Balance");
-const $Categoria = $("#Categoria");
-const $Reporte= $("#Reporte");
+const $Balance = $(".Balance");
+const $Categoria = $(".Categoria");
+
+const $Reporte= $(".Reporte");
 const $seccionBalance =$("#seccionBalance")
 
 const $seccionFiltros=$("#seccionFiltros")
@@ -24,6 +38,8 @@ const $seccionCategoria =$("#seccionCategoria")
 const $seccionReporte =$("#seccionReporte")
 const $seccionNuevaOperacion= $("#seccionNuevaOperacion")
 const $nuevaOperacion = $("#nuevaOperacion")
+const $Fitros = $("#Fitros")
+
 
 
 
@@ -37,6 +53,7 @@ $Balance.addEventListener("click", () => {
       $seccionCategoria.style.display = "none";
       $seccionReporte.style.display = "none";
 })
+
 $Categoria.addEventListener("click", () => {
       $seccionCategoria.style.display = "block";
       $seccionBalance.style.display = "none";
@@ -63,8 +80,37 @@ $nuevaOperacion.addEventListener("click", () => {
       $seccionFiltros.style.display = "none";
       
       $seccionOperaciones.style.display = "none";
-      const $contenedorOperaciones = $("#contenedorOperaciones")
+    
      
 })
+//$Fitros.addEventListener("input"()=>{
+ //     const montoFiltrado = datos.filter(e=> )
+//})
 
-    
+
+function pintarDatos(){
+$seccionOperaciones.innerHTML += `<div id="seccionOperaciones"
+class="hidden Operaciones Balance   bg-white rounded-2xl  shadow-xl    m-[20px] p-[15px] w-11/12  ">
+<div class="  justify-between lg:text-2xl md:flex">
+      <h2 class="operaciones  ">Operaciones</h2>
+      <button id="nuevaOperacion" class="  bg-green-200 p-[10px] m-2 "> + Nueva operacion</button>
+</div>
+<div id="contenedorOperaciones"
+      class=" flex-col  imagenNuevaOperacion bg-[./Style/img/imagenNuevoGasto] bg-gray-500 h-[400px] w-11/12 text-lg p-8 ">
+      <div class="flex-col justify-between ">
+            <button class="text-base">${datos.Categoria}</button>
+            <button class="text-base">trabajo</button>
+      </div>
+      <div class="flex-col justify-between">
+            <span>$100000</span>
+            <button class="text-xs">Editar</button>
+            <button class="text-xs">Eliminar</button>
+      </div>
+</div>
+<div class="flex flex-col items-center">
+      <h2 class="flex   text-2xl">Sin Resultados</h2>
+      <p class=" flex justify-center m-[40px]">Cambia los filtros o agrega operaciones</p>
+</div>
+
+</div>`
+}
